a63991ba7b1ac4281278255de85045c4afb2ad20
Lala5th <lala5th@gmail.com>
2020-10-19 02:10:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+This is a discord bot that checks Imperial secrets for new secrets and posts them throughout the hour, every hour. Due to security API keys and access tokens are not included.
---
+++ bot.js
@@ -0,0 +1,100 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const request = require('request');
+const fs = require('fs')
+const auth = require('./auth.json');
+const TOKEN = auth.token;
+
+const API_call_timeout = 3600000;
+
+bot.login(TOKEN);
+
+channels = [];
+
+channel_data = require('./channels.json');
+
+var secret_num = require('./progress.json').secret_num;
+var secrets = [];
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+    console.log("Started initialisation.");
+    for(c in channel_data){
+        bot.channels.fetch(channel_data[c].id).then(channel => {
+            channels.push(channel);
+        }).catch(console.error);
+    }
+    console.log("Finished initialisation.");
+});
+
+bot.on('message', msg => {
+    switch(msg.content){
+        case '!_Add':
+            channels.push(msg.channel);
+            console.log("Added new channel: " + msg.channel.name);
+            break;
+        case '!_Remove':
+            let found = false;
+            for(i = 0; i < channels.length; i++){
+                if(found && i != channels.length){
+                    channels[i] = channels[i+1]
+                }else if(channels[i] == msg.channel){
+                    found = true;
+                    if(i != channels.length){
+                        channels[i] = channels[i+1]
+                    }
+                }
+            }
+            if(found){
+                channels.pop();
+            }
+            console.log("Removed channel: " + msg.channel.name)
+            break;
+    }
+});
+
+function postSecret(){
+    if(secrets.length == 0){
+        return;
+    }
+    let s = secrets[secrets.length - 1];
+    secret_num = s.imperialSecretNumber;
+    console.log("Posting Secret:" + s.imperialSecretNumber + "\n" + s.prettifiedSecret);
+    for(c in channels){
+        channels[c].send("Imperial Secret:" + s.imperialSecretNumber + "\n" + s.prettifiedSecret);
+    }
+    secrets.pop();
+}
+
+// Grab secrets using API
+setInterval(() => {
+    let user = auth.db_user;
+    let pass = auth.db_pass;
+    let auth = "Basic " + new Buffer(user + ":" + pass).toString("base64");
+    console.log("Started API call");
+    request({ 'url' : auth.url, 'headers' : {
+        "Authorization" : auth
+    }, 'json' : true}, (err, resp, body) => {
+        console.error(err);
+        secrets = body;
+        while(true){
+            if(secrets.length == 0){
+                break;
+            }
+            if(secrets[secrets.length - 1].imperialSecretNumber <= secret_num){
+                secrets.pop();
+            }else{
+                break;
+            }
+        }
+    })
+    let interval = API_call_timeout/(secrets.length + 1);
+    setInterval(() => {postSecret()},interval);
+},API_call_timeout);
+
+// Save active channels
+setInterval(() => {
+    fs.writeFileSync('./channels.json', JSON.stringify(channels, null, 4), 'utf-8');
+    fs.writeFileSync('./progress.json', JSON.stringify({"secret_num" : secret_num}, null, 4), 'utf-8');
+},10000);
